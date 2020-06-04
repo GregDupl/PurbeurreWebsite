bc1d0e74ade0d0694ac440b753761fa22cc401da
GregDupl <gdu.contact@gmail.com>
2020-06-04 16:13:26 +0200

    separation env prod and dev + modification travis

--- purbeurre_project/settings/__init__.py
+++ purbeurre_project/settings/__init__.py
@@ -24,8 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mo*nh4r^sqi#$t_g!fk!9s381%(j3_$p_3^)=yuezfop@zi47y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 
-ALLOWED_HOSTS = ['purbeurreappli.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -82,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'purbeurre',
-        'USER': 'postgres',
+        'USER': 'greg',
         'PASSWORD': '',
         'HOST': '',
         'PORT': '5432',
@@ -126,24 +127,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-if os.environ.get('ENV') == 'PRODUCTION':
-    DEBUG = False
-    # Static files settings
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
-
-else:
-    DEBUG = True
