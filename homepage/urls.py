7602d7030829dcc489655a9f5e418cc9c6c38c54
GregDupl <gdu.contact@gmail.com>
2020-07-21 16:54:24 +0200

    delete url email and name

--- homepage/urls.py
+++ homepage/urls.py
@@ -18,7 +18,5 @@ urlpatterns = [
     url(r"^legal$", views.legal, name="legal"),
     url(r"^delete/(?P<id>[0-9]+)/$", views.delete, name="delete"),
     url(r"^update", views.update, name="update"),
-    url(r"^email", views.newmail, name="newmail"),
-    url(r"^name", views.newname, name="newname"),
     url(r"^pass", views.newpass, name="newpass"),
 ]
